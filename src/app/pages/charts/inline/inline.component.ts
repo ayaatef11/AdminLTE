ce105d8bc0eaf8607411a0d96d7424b7f75a215f
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 21:57:48 +0200

    add inline component

---
+++ src/app/pages/charts/inline/inline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline',
+  standalone: true,
+  imports: [],
+  templateUrl: './inline.component.html',
+  styleUrl: './inline.component.css'
+})
+export class InlineComponent {
+
+}
