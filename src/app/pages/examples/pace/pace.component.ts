9aa0e0b60c2d6a1606912699ac2faa23147e8245
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:03:10 +0200

    add pace component

---
+++ src/app/pages/examples/pace/pace.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pace',
+  standalone: true,
+  imports: [],
+  templateUrl: './pace.component.html',
+  styleUrl: './pace.component.css'
+})
+export class PaceComponent {
+
+}
