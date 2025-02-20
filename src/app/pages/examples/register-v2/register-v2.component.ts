1af6dc6f4018c485113b87f49dd01c67e2ea016f
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:06:56 +0200

    add register-v2 component

---
+++ src/app/pages/examples/register-v2/register-v2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-v2',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-v2.component.html',
+  styleUrl: './register-v2.component.css'
+})
+export class RegisterV2Component {
+
+}
