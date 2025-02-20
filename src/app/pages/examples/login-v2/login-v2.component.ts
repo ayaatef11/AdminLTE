ff05d14520065242abf784455b19b8e7a44b56f1
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:02:33 +0200

    add login-v2 component

---
+++ src/app/pages/examples/login-v2/login-v2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-v2',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-v2.component.html',
+  styleUrl: './login-v2.component.css'
+})
+export class LoginV2Component {
+
+}
