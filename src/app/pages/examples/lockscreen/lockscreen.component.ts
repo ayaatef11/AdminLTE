bd3dd56f7a690c48eeb72b7373b319ee42f3409d
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:01:53 +0200

    add lockscreen component

---
+++ src/app/pages/examples/lockscreen/lockscreen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lockscreen',
+  standalone: true,
+  imports: [],
+  templateUrl: './lockscreen.component.html',
+  styleUrl: './lockscreen.component.css'
+})
+export class LockscreenComponent {
+
+}
