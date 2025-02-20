a9ee38a47eb08b8ca3bbc92ba46e2dd75baf654e
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:06:08 +0200

    add recover-password-v2 component

---
+++ src/app/pages/examples/recover-password-v2/recover-password-v2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover-password-v2',
+  standalone: true,
+  imports: [],
+  templateUrl: './recover-password-v2.component.html',
+  styleUrl: './recover-password-v2.component.css'
+})
+export class RecoverPasswordV2Component {
+
+}
