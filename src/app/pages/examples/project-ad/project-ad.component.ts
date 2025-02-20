0815d0fc6646f542fed8d78c29f740937f1f5d7b
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:03:37 +0200

    add project-ad component

---
+++ src/app/pages/examples/project-ad/project-ad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-ad',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-ad.component.html',
+  styleUrl: './project-ad.component.css'
+})
+export class ProjectAdComponent {
+
+}
