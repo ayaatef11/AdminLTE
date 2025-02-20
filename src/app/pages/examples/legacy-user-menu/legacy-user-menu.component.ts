12e99ec987948bd4ba6cb8770b9d7df7bdf07ddf
ayaatef11 <ayabadrin667@gmail.com>
2025-02-20 22:01:38 +0200

    add legacy-user-menu component

---
+++ src/app/pages/examples/legacy-user-menu/legacy-user-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legacy-user-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './legacy-user-menu.component.html',
+  styleUrl: './legacy-user-menu.component.css'
+})
+export class LegacyUserMenuComponent {
+
+}
