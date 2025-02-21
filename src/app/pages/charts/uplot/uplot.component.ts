15e7d62ad0a23c59ae6f5cf87e97b64694fa9472
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 09:27:51 +0200

    add uplot head styles

--- src/app/pages/charts/uplot/uplot.component.ts
+++ src/app/pages/charts/uplot/uplot.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './uplot.component.css'
 })
 export class UplotComponent {
+  constructor() {}
 
+  ngOnInit(): void {
+    this.loadStylesheets([
+      "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback", // Google Font
+      "../../plugins/fontawesome-free/css/all.min.css", // Font Awesome
+      "../../plugins/uplot/uPlot.min.css", // uPlot
+      "../../dist/css/adminlte.min.css", // AdminLTE Theme
+    ]);
+  }
+
+  private loadStylesheets(urls: string[]): void {
+    urls.forEach((url) => {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+    });
+  }
 }
