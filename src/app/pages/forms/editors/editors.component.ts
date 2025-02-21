cf8b1b2b8a1820be04cfc2f99f76afb90eae1f8e
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 02:55:29 +0200

    add editors component

---
+++ src/app/pages/forms/editors/editors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editors',
+  standalone: true,
+  imports: [],
+  templateUrl: './editors.component.html',
+  styleUrl: './editors.component.css'
+})
+export class EditorsComponent {
+
+}
