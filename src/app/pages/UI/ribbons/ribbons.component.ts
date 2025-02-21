f37d91e3c605cf741a5a49c775cbb0fe521910f6
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 03:06:56 +0200

    add ribbons component

---
+++ src/app/pages/UI/ribbons/ribbons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ribbons',
+  standalone: true,
+  imports: [],
+  templateUrl: './ribbons.component.html',
+  styleUrl: './ribbons.component.css'
+})
+export class RibbonsComponent {
+
+}
