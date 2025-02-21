fe6f886c4159ffb2aa89b5683453f2ae9a29f0d8
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 02:57:39 +0200

    add boxed component

---
+++ src/app/pages/layout/boxed/boxed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boxed',
+  standalone: true,
+  imports: [],
+  templateUrl: './boxed.component.html',
+  styleUrl: './boxed.component.css'
+})
+export class BoxedComponent {
+
+}
