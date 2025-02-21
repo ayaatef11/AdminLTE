7c4c503cda5af67fb2e0d698b32b2fa25ba20e27
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 02:58:38 +0200

    add fixed-footer component

---
+++ src/app/pages/layout/fixed-footer/fixed-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './fixed-footer.component.html',
+  styleUrl: './fixed-footer.component.css'
+})
+export class FixedFooterComponent {
+
+}
