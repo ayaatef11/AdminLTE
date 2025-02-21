35d5db6b6a9b33b24165a12d366f515cb5a7d431
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 09:32:10 +0200

    add calender head style

--- src/app/pages/calender/calender.component.ts
+++ src/app/pages/calender/calender.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './calender.component.css'
 })
 export class CalenderComponent {
+  ngOnInit(): void {
+    this.loadStylesheets([
+      "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback", // Google Font
+      "../plugins/fontawesome-free/css/all.min.css", // Font Awesome
+      "../plugins/fullcalendar/main.css", // FullCalendar
+      "../dist/css/adminlte.min.css", // AdminLTE Theme
+    ]);
+  }
 
+  private loadStylesheets(urls: string[]): void {
+    urls.forEach((url) => {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+    });
+  }
 }
