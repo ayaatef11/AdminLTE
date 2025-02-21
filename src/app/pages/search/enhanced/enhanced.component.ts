2fb5db8f6cb62197209fd7c8261ba41fab5226b0
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 03:01:39 +0200

    add enhanced component

---
+++ src/app/pages/search/enhanced/enhanced.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enhanced',
+  standalone: true,
+  imports: [],
+  templateUrl: './enhanced.component.html',
+  styleUrl: './enhanced.component.css'
+})
+export class EnhancedComponent {
+
+}
