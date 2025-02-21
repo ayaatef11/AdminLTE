6dd9b9b985a9b044584195d0f899a0b1339881e0
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 08:57:22 +0200

    add chartjs body part

--- src/app/pages/charts/chartjs/chartjs.component.ts
+++ src/app/pages/charts/chartjs/chartjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-chartjs',
@@ -8,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './chartjs.component.css'
 })
 export class ChartjsComponent {
+  //load fonts and styles dynamically
+  ngOnInit() {
+    // Load Google Font
+    const fontLink = document.createElement('link');
+    fontLink.rel = 'stylesheet';
+    fontLink.href = 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback';
+    document.head.appendChild(fontLink);
 
+    // Load Font Awesome
+    const faLink = document.createElement('link');
+    faLink.rel = 'stylesheet';
+    faLink.href = '../../plugins/fontawesome-free/css/all.min.css';
+    document.head.appendChild(faLink);
+
+    // Load AdminLTE Theme
+    const themeLink = document.createElement('link');
+    themeLink.rel = 'stylesheet';
+    themeLink.href = '../../dist/css/adminlte.min.css';
+    document.head.appendChild(themeLink);
+  }
+
+  constructor(private titleService: Title, private metaService: Meta) {
+    // Set Page Title
+    this.titleService.setTitle('AdminLTE 3 | ChartJS');
+
+    // Set Meta Tags
+    this.metaService.updateTag({ name: 'viewport', content: 'width=device-width, initial-scale=1' });
+    this.metaService.updateTag({ charset: 'utf-8' });
+}
 }
