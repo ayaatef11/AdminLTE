64259e91b1f35a3c57ff6730ec5e0307bbb33053
ayaatef11 <ayabadrin667@gmail.com>
2025-02-21 09:22:21 +0200

    add styles for inline page

--- src/app/pages/charts/inline/inline.component.ts
+++ src/app/pages/charts/inline/inline.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inline',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './inline.component.css'
 })
 export class InlineComponent {
-
+  constructor(private titleService:Title,private metaService:Meta){
+this.titleService.setTitle('AdminLTE | Inline Charts');
+this.metaService.updateTag({name:"viewport",content:'width=devicee-width , initial-scale=1' , charset="utf-8"})
+   const fontLink=document.createElement('link');
+   fontLink.rel="stylesheet";
+   fontLink.href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback";
+    const faLink=document.createElement('link');
+    faLink.rel="stylesheet";
+    faLink.href="../../plugins/fontawesome-free/css/all.min.css"
+const adLink=document.createElement('link');
+adLink.rel="stylesheet";
+adLink.href="../../dist/css/adminlte.min.css";
+}
 }
