eff1d0f0df25099f1d6dbb16c2782e15d6c526f4
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 03:17:19 +0200

    blank head style

--- src/app/pages/examples/blank/blank.component.ts
+++ src/app/pages/examples/blank/blank.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './blank.component.css'
 })
 export class BlankComponent {
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loadStylesheets([
+      "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback", // Google Font
+      "../../plugins/fontawesome-free/css/all.min.css", // Font Awesome
+      "../../dist/css/adminlte.min.css", // AdminLTE Theme
+    ]);
+  }
+
+  private loadStylesheets(urls: string[]): void {
+    urls.forEach((url) => {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+    });
+  }
 
 }
