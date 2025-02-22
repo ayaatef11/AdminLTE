6c677e021a097a5b6714194304bf78edd90f6f44
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 07:45:33 +0200

    add gallery head style

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.loadStylesheets();
+  }
+
+  loadStylesheets() {
+    this.addStylesheet('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback');
+    this.addStylesheet('/plugins/fontawesome-free/css/all.min.css');
+    this.addStylesheet('/plugins/ekko-lightbox/ekko-lightbox.css');
+    this.addStylesheet('/dist/css/adminlte.min.css');
+  }
+
+  addStylesheet(href: string) {
+    const link = this.renderer.createElement('link');
+    this.renderer.setAttribute(link, 'rel', 'stylesheet');
+    this.renderer.setAttribute(link, 'href', href);
+    this.renderer.appendChild(document.head, link);
+  }
 
 }
