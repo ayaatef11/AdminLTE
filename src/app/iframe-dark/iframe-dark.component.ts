9efc42f664f3154b6b3e7f639158ce05573bc1f0
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 07:39:23 +0200

    add iframe head style

--- src/app/iframe-dark/iframe-dark.component.ts
+++ src/app/iframe-dark/iframe-dark.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-iframe-dark',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './iframe-dark.component.html',
   styleUrl: './iframe-dark.component.css'
 })
-export class IframeDarkComponent {
+export class IframeDarkComponent implements OnInit {
 
+    constructor(private renderer: Renderer2) { }
+
+    ngOnInit(): void {
+      this.addStylesheet('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback');
+      this.addStylesheet('/plugins/fontawesome-free/css/all.min.css');
+      this.addStylesheet('/dist/css/adminlte.min.css');
+      this.addStylesheet('/plugins/overlayScrollbars/css/OverlayScrollbars.min.css');
+    }
+
+    private addStylesheet(href: string): void {
+      const link = this.renderer.createElement('link');
+      this.renderer.setAttribute(link, 'rel', 'stylesheet');
+      this.renderer.setAttribute(link, 'href', href);
+      this.renderer.appendChild(document.head, link);
+    }
 }
