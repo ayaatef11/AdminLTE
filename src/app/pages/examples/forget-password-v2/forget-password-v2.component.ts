cf58e1d683c268e0aca04f07a3329c5a0ca56b0a
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 03:18:55 +0200

    forget password version 2 style

--- src/app/pages/examples/forget-password-v2/forget-password-v2.component.ts
+++ src/app/pages/examples/forget-password-v2/forget-password-v2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ForgetPasswordService } from '../forget-password/forget-password.service';
 
 @Component({
   selector: 'app-forget-password-v2',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './forget-password-v2.component.css'
 })
 export class ForgetPasswordV2Component {
+constructor(private forgetPassword:ForgetPasswordService){}
+ngOnInit(){
+this.forgetPassword.updateTitle("AdminLTE 3 | Forgot Password (v2)")
+this.forgetPassword.updateMeta('viewport', 'width=device-width, initial-scale=1');
+    this.forgetPassword.updateMeta('charset', 'utf-8');
 
-}
+    // Add Stylesheets
+    this.forgetPassword.addStylesheet('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback');
+    this.forgetPassword.addStylesheet('../../plugins/fontawesome-free/css/all.min.css');
+    this.forgetPassword.addStylesheet('../../plugins/icheck-bootstrap/icheck-bootstrap.min.css');
+    this.forgetPassword.addStylesheet('../../dist/css/adminlte.min.css');
+
+}}
