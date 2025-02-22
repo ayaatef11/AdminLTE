29dfa0e473bf8a0d25833dff7fd1f542b391aec2
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 07:40:05 +0200

    add language menu head style

--- src/app/pages/examples/language-menu/language-menu.component.ts
+++ src/app/pages/examples/language-menu/language-menu.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './language-menu.component.css'
 })
 export class LanguageMenuComponent {
+  constructor(private forgetPassword:ForgetPasswordService){}
+  ngOnInit(){
+  this.forgetPassword.updateTitle("AdminLTE 3 | Language Menu")
+  this.forgetPassword.updateMeta('viewport', 'width=device-width, initial-scale=1');
+      this.forgetPassword.updateMeta('charset', 'utf-8');
 
+      // Add Stylesheets
+      this.forgetPassword.addStylesheet('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback');
+      this.forgetPassword.addStylesheet('/plugins/fontawesome-free/css/all.min.css');
+      this.forgetPassword.addStylesheet('/plugins/flag-icon-css/css/flag-icon.min.css');
+      this.forgetPassword.addStylesheet('/dist/css/adminlte.min.css');
+  }
 }
