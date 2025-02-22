0f255fb83f6540425d6d18f236a7f23ac84aa45d
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 03:17:39 +0200

    contact-us head style

--- src/app/pages/examples/contact-us/contact-us.component.ts
+++ src/app/pages/examples/contact-us/contact-us.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+  constructor() {}
 
+  ngOnInit(): void {
+    this.loadStylesheets([
+      "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback", // Google Font
+      "../../plugins/fontawesome-free/css/all.min.css", // Font Awesome
+      "../../dist/css/adminlte.min.css", // AdminLTE Theme
+    ]);
+  }
+
+  private loadStylesheets(urls: string[]): void {
+    urls.forEach((url) => {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+    });
+  }
 }
