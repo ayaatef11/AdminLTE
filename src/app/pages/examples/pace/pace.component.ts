72e09c05a664201f6a85ee39d432dddab1c47c64
ayaatef11 <ayabadrin667@gmail.com>
2025-02-22 07:41:25 +0200

    add pace page head style

--- src/app/pages/examples/pace/pace.component.ts
+++ src/app/pages/examples/pace/pace.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './pace.component.css'
 })
 export class PaceComponent {
+constructor(private forgetPassword:ForgetPasswordService){}
+ngOnInit(){
+this.forgetPassword.updateTitle("AdminLTE 3 | Forgot Password (v2)")
+this.forgetPassword.updateMeta('viewport', 'width=device-width, initial-scale=1');
+    this.forgetPassword.updateMeta('charset', 'utf-8');
+
+    // Add Stylesheets
+    this.forgetPassword.addStylesheet('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback');
+    this.forgetPassword.addStylesheet('/plugins/fontawesome-free/css/all.min.css');
+    this.forgetPassword.addStylesheet('/plugins/icheck-bootstrap/icheck-bootstrap.min.css');
+    this.forgetPassword.addStylesheet('/dist/css/adminlte.min.css');
+    this.forgetPassword.addStylesheet('/plugins/pace-progress/themes/black/pace-theme-flat-top.css');
+
+}}
 
 }
